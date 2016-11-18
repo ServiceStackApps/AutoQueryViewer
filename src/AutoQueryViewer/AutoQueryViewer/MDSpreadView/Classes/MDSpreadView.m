dfba3f54e8c0d6f4d14c1be6a2a14ffdbf2b7b0d
Demis Bellot <demis.bellot@gmail.com>
2016-11-17 22:21:46 -0500

    Fix remaining warnings

--- src/AutoQueryViewer/AutoQueryViewer/MDSpreadView/Classes/MDSpreadView.m
+++ src/AutoQueryViewer/AutoQueryViewer/MDSpreadView/Classes/MDSpreadView.m
@@ -482,7 +482,7 @@ static CGFloat MDPixel()
 
 - (BOOL)isEqualToIndexPath:(MDIndexPath *)object
 {
-    if (object == nil) return nil;
+    if (object == nil) return false;
     return (object->section == self->section && object->row == self->row);
 }
 
